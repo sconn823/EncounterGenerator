c91700f0b366630c97a8aa3390014520d34f2fb9
Sean <sconn823@gmail.com>
2019-05-06 09:24:43 -0400

    Saving my changes to the git repo

--- parseJSON.py
+++ parseJSON.py
@@ -1,9 +1,16 @@
 import json
 import requests
 
-
 with open("monsters.json") as file:
     monsterList = json.load(file)
 
+finishedList = []
 for obj in monsterList["results"]:
-    print(obj)
+    print(obj["url"])
+
+    finishedList.append(
+        {obj["name"]: requests.get(obj["url"]).content}
+    )
+
+with open("finishedList.json", "w") as output:
+    json.dump(finishedList, output)
