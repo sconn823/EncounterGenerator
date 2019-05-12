8214f8838121d9acc0bd4c454cc1cbf43c02a4ad
Sean <sconn823@gmail.com>
2019-05-12 17:55:27 -0400

    Need to fix error with hwo enemies are selected.

---
+++ encounterGenerator.py
@@ -0,0 +1,42 @@
+import json
+import random
+
+groupRating = 10
+
+
+def fixList(fullList, currentRating):
+    newList = []
+    for number in fullList:
+        if(float(number) <= currentRating):
+            newList.append(number)
+    return newList
+
+
+def findMonster(targetRating, monsterJSON):
+    monsterOptions = []
+    for obj in monsterJSON[int(targetRating)]:
+        monsterOptions.append(obj)
+
+    chosenMonster = random.choice(monsterOptions)
+    print(chosenMonster)
+    return(chosenMonster)
+
+
+with open('crSortedMonsters.json', 'r') as input:
+    sortedList = json.load(input)
+
+sortedList = fixList(sortedList, groupRating)
+challengeRatings = []
+for obj in sortedList:
+    if float(obj) > 0:
+        challengeRatings.append(obj)
+
+encounterList = []
+while groupRating > 0:
+    targetRating = random.choice(challengeRatings)
+    encounterList.append(findMonster(targetRating, sortedList))
+    groupRating -= float(targetRating)
+
+for obj in encounterList:
+    #print(obj["name"] + " - " + obj["challenge_rating"])
+    print(obj)
