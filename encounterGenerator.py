fab7061181a891aac50eabee2860f31c47d7d7c7
Sean <sconn823@gmail.com>
2019-05-17 16:27:44 -0400

    Can now select and build encoutners

--- encounterGenerator.py
+++ encounterGenerator.py
@@ -5,20 +5,19 @@ groupRating = 10
 
 
 def fixList(fullList, currentRating):
-    newList = []
+    newList = {}
     for number in fullList:
         if(float(number) <= currentRating):
-            newList.append(number)
+            newList.update({number: fullList[number]})
     return newList
 
 
 def findMonster(targetRating, monsterJSON):
     monsterOptions = []
-    for obj in monsterJSON[int(targetRating)]:
+    for obj in monsterJSON[targetRating]:
         monsterOptions.append(obj)
 
     chosenMonster = random.choice(monsterOptions)
-    print(chosenMonster)
     return(chosenMonster)
 
 
@@ -36,6 +35,11 @@ while groupRating > 0:
     targetRating = random.choice(challengeRatings)
     encounterList.append(findMonster(targetRating, sortedList))
     groupRating -= float(targetRating)
+    fixList(sortedList, groupRating)
 
+sum = 0
 for obj in encounterList:
-    print(obj)
+    print(obj['name'] + " / " + str(obj['challenge_rating']))
+    sum += float(obj['challenge_rating'])
+
+print("Final rating: " + str(sum))
